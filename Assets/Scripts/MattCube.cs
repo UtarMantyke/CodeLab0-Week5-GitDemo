5fec1e56c5bec35e77876b06cf405955d461c47c
Amber <gl1697@gmail.com>
2018-10-05 11:12:20 -0400

    Added a cube and a script

---
+++ Assets/Scripts/MattCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MattCube : MonoBehaviour {
+	public Vector3 cubeSpeed = new Vector3().up;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
